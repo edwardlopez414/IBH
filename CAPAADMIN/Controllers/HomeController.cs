b4ea53c60364eae1328e191e8e38d5c7fcec8b8d
Edward Enrique Lopez Perez <edward.lopez@info.grupocoen.com>
2023-05-22 18:37:26 -0500

    cargando plantilla

---
+++ CAPAADMIN/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CAPAADMIN.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult reportes()
+        {
+            return View();
+        }
+
+        public ActionResult reporteevento()
+        {
+            return View();
+        }
+        public ActionResult Activity()
+        {
+            return View();
+        }
+
+        public ActionResult Cataloguser()
+        {
+            return View();
+        }
+        public ActionResult deleteuser()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CAPAADMIN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CAPAADMIN.MvcApplication" Language="C#" %>

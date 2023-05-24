7097a382cb9c0271447c663cc3f5ec9617d7097a
Barakielop <barakielop002@gmail.com>
2023-05-23 22:16:46 -0700

    desarollo dashboard v1

--- CAPAADMIN/Scripts/metadata.js
+++ CAPAADMIN/Scripts/metadata.js
@@ -31,14 +31,14 @@ new Chart($grafica, {
 });
 
 // Obtener una referencia al elemento canvas del DOM
-const $grafica = document.querySelector("#grafica2");
+const $grafica2 = document.querySelector("#grafica2");
 // Las etiquetas son las que van en el eje X. 
-const etiquetas = ["Enero", "Febrero", "Marzo", "Abril"]
+const etiquetas2 = ["Enero", "Febrero", "Marzo", "Abril"]
 // Podemos tener varios conjuntos de datos. Comencemos con uno
 const datosVentas2021 = {
     label: "Ventas por mes",
     data: [5000, 1500, 8000, 5102], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
     borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
     borderWidth: 1,// Ancho del borde
 };
